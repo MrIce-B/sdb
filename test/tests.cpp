8130a2c2ed5d2b76463517782c00b456bfc42508
MrIce-B <1873545927@qq.com>
2025-06-27 15:50:34 +0800

    The first commit of chapter4

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
+#include <libsdb/Process.hpp>
 
-TEST_CASE("validate environment") {
-    REQUIRE(true);
+using namespace sdb;
+
+namespace{
+    bool process_exists(pid_t pid);
+}
+
+TEST_CASE("Porcess::launch success", "[Process]") {
+    auto proc = Process::launch("yes");
+    REQUIRE(process_exists(proc->pid()));
 }
